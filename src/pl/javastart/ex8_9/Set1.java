ec3493bc827e4747516f47b24e21bdd930cd456f
Ryszard Szymanski <ryszymanski@gmail.com>
2017-01-26 18:09:43 +0100

    Poprawione zad 2.

--- src/pl/javastart/ex8_9/Set1.java
+++ src/pl/javastart/ex8_9/Set1.java
@@ -13,16 +13,20 @@ public class Set1 {
 
         TreeSet treeSet = new TreeSet();
         Random random = new Random();
-        int setSize = 50;
         int sum = 0;
         double avarage = 0;
 
-        for (int i = 0; i < setSize; i++) {
+        while (treeSet.size()<50){
             int input = (random.nextInt(1000));
             treeSet.add(input);
-            sum = sum + input;
         }
-        avarage = sum/setSize;
+
+        for (Object o : treeSet) {
+            sum = sum + (int)o;
+        }
+
+        System.out.println(treeSet.size());
+        avarage = sum/treeSet.size();
         System.out.println("Najmniejsza liczba to: "+treeSet.first());
         System.out.println("Największa liczba to: "+treeSet.last());
         System.out.println("Średnia wartość wynosi: "+avarage);
